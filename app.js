1c1739517db206fc1999b5b7a3e1374a8dc84d8c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 12:13:13 +0500

    worrking

--- app.js
+++ app.js
@@ -142,24 +142,24 @@ let getWeather = (name, lat, lon) => {
         })
 }
 
-function getCityCoordinates() {
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            const { latitude, longitude } = position.coords; // Get coordinates of user location
-            // Get city name from coordinates using reverse geocoding API
-            const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=6a6cb112b4746fd1d963422db62a0782&q`;
-            fetch(API_URL).then(response => response.json()).then(data => {
-                const { name } = data[0];
-                getWeatherDetails(name, latitude, longitude);
-            }).catch(() => {
-                alert("An error occurred while fetching the city name!");
-            });
-        },
-        error => { // Show alert if user denied the location permission
-            if (error.code === error.PERMISSION_DENIED) {
-                alert("Geolocation request denied. Please reset location permission to grant access again.");
-            } else {
-                alert("Geolocation request error. Please reset location permission.");
-            }
-        });
-}
+// function getCityCoordinates() {
+//     navigator.geolocation.getCurrentPosition(
+//         position => {
+//             const { latitude, longitude } = position.coords; // Get coordinates of user location
+//             // Get city name from coordinates using reverse geocoding API
+//             const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=6a6cb112b4746fd1d963422db62a0782&q`;
+//             fetch(API_URL).then(response => response.json()).then(data => {
+//                 const { name } = data[0];
+//                 getWeatherDetails(name, latitude, longitude);
+//             }).catch(() => {
+//                 alert("An error occurred while fetching the city name!");
+//             });
+//         },
+//         error => { // Show alert if user denied the location permission
+//             if (error.code === error.PERMISSION_DENIED) {
+//                 alert("Geolocation request denied. Please reset location permission to grant access again.");
+//             } else {
+//                 alert("Geolocation request error. Please reset location permission.");
+//             }
+//         });
+// }
