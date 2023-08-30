559ebe071969b08cd2722cd73cc3234f73919428
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-08-30 12:53:56 +0500

    reset code

--- app.js
+++ app.js
@@ -1,159 +1,125 @@
-let getWeather = () => {
+const weatherBackgrounds = {
+    Clear: 'clear.jpg',
+    Clouds: {
+        801: 'clouds801.jpg',
+        802: 'clouds802.jpg',
+        803: 'clouds803.jpg',
+        804: 'clouds804.jpg'
+    },
+    Haze: 'haze.jpg',
+    Fog: 'fog.jpg',
+    Mist: 'mist.jpg',
+    Smoke: 'smoke.jpg',
+    Snow: {
+        600: 'light_snow.jpg',
+        601: 'light_snow.jpg', // Using the same image for 600 and 601
+        602: 'snow.jpg',
+        611: 'snow.jpg', // Using the same image for various snow conditions
+        612: 'snow.jpg',
+        613: 'snow.jpg',
+        615: 'snow_rain.jpg',
+        616: 'snow_rain.jpg', // Using the same image for 615 and 616
+        620: 'snow_shower.jpg',
+        621: 'snow_shower.jpg',
+        622: 'snow_shower.jpg'
+    },
+    Rain: {
+        500: 'rain.jpg',
+        501: 'rain.jpg',
+        502: 'heavyrain.jpg',
+        503: 'heavyrain.jpg',
+        504: 'heavyrain.jpg',
+        520: 'heavyrain.jpg',
+        521: 'heavyrain.jpg',
+        522: 'heavyrain.jpg',
+        531: 'heavyrain.jpg',
+        511: 'snow_rain.jpg'
+    },
+    Thunderstorm: {
+        200: 'thunderRain.jpg',
+        201: 'thunderRain.jpg',
+        202: 'thunderRain.jpg',
+        210: 'thunder.jpg',
+        211: 'thunder.jpg',
+        212: 'heavyThunder.jpg',
+        221: 'heavyThunder.jpg',
+        230: 'heavyThunder.jpg',
+        231: 'heavyThunder.jpg',
+        232: 'heavyThunder.jpg'
+    },
+    Drizzle: 'rain.jpg',
+    Default: 'base.jpg'
+};
 
-    let body = document.querySelector("body");
+const apiKey = '6a6cb112b4746fd1d963422db62a0782';
 
-    navigator.geolocation.getCurrentPosition(async (success) => {
-        const { latitude, longitude } = success.coords;
+const getWeather = async () => {
+    try {
+        const body = document.querySelector("body");
+        const location = await getCurrentLocation();
+        const weatherData = await fetchWeatherData(location.latitude, location.longitude);
+        updateWeatherDetails(weatherData);
+        updateBackground(weatherData.weather[0].main, weatherData.weather[0].id);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+};
+
+const getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (success) => {
+                const { latitude, longitude } = success.coords;
+                resolve({ latitude, longitude });
+            },
+            (error) => {
+                reject(error);
+            }
+        );
     });
+};
 
-    axios.get(`https://api.openweathermap.org/data/2.5//forecast?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=6a6cb112b4746fd1d963422db62a0782&q=${cityName}`)
-        .then(function (response) {
-            console.log("response is success");
-            console.log(response.data);
+const fetchWeatherData = async (latitude, longitude) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("Failed to fetch weather data");
+    }
+    return await response.json();
+};
 
-            const currentWeather = `<div class="details" >
-                                    <h2>${cityName} ( ${response.data.weather[0].description} )</h2>
-                                    <h6>Temperature: ${response.data.main.temp}°C</h6>
-                                    <h6>Feels Like: ${response.data.main.feels_like}°C</h6>
-                                    <h6>Max Temperature: ${response.data.main.temp_max}°C</h6>
-                                    <h6>Min Temperature: ${response.data.main.temp_min}°C</h6>
-                                    <h6>Wind: ${response.data.wind.speed} M/S</h6>
-                                    <h6>Humidity: ${response.data.main.humidity}%</h6>
-                                    </div>`;
+const updateWeatherDetails = (weatherData) => {
+    const cityName = weatherData.name;
+    const description = weatherData.weather[0].description;
+    const main = weatherData.main;
+    const wind = weatherData.wind;
 
-            document.querySelector('.current-weather').innerHTML = currentWeather;
+    const currentWeather = `
+        <div class="details">
+            <h2>${cityName} (${description})</h2>
+            <h6>Temperature: ${main.temp}°C</h6>
+            <h6>Feels Like: ${main.feels_like}°C</h6>
+            <h6>Max Temperature: ${main.temp_max}°C</h6>
+            <h6>Min Temperature: ${main.temp_min}°C</h6>
+            <h6>Wind: ${wind.speed} M/S</h6>
+            <h6>Humidity: ${main.humidity}%</h6>
+        </div>`;
 
-            let weatherMain = response.data.weather[0].main;
+    document.querySelector('.current-weather').innerHTML = currentWeather;
+};
 
-            if (weatherMain == 'Clear') {
-                body.style.backgroundImage = 'url("Images/clear.jpg")';
-            }
-            else if (weatherMain == 'Clouds') {
-                let weatherId = response.data.weather[0].id;
-                switch (weatherId) {
-                    case 801:
-                        body.style.backgroundImage = 'url("Images/clouds801.jpg")';
-                        break;
-                    case 802:
-                        body.style.backgroundImage = 'url("Images/clouds802.jpg")';
-                        break;
-                    case 803:
-                        body.style.backgroundImage = 'url("Images/clouds803.jpg")';
-                        break;
-                    case 804:
-                        body.style.backgroundImage = 'url("Images/clouds804.jpg")';
-                        break;
-                }
-            }
-            else if (weatherMain == 'Haze') {
-                body.style.backgroundImage = 'url("Images/haze.jpg")';
-            }
-            else if (weatherMain == 'Fog') {
-                body.style.backgroundImage = 'url("Images/fog.jpg")';
-            }
-            else if (weatherMain == 'Mist') {
-                body.style.backgroundImage = 'url("Images/mist.jpg")';
-            }
-            else if (weatherMain == 'Smoke') {
-                body.style.backgroundImage = 'url("Images/smoke.jpg")';
-            }
-            else if (weatherMain == 'Snow') {
-                let weatherId = response.data.weather[0].id;
-                switch (weatherId) {
-                    case 600:
-                    case 601:
-                        body.style.backgroundImage = 'url("Images/light_snow.jpg")';
-                        break;
-                    case 602:
-                    case 611:
-                    case 612:
-                    case 613:
-                        body.style.backgroundImage = 'url("Images/snow.jpg")';
-                        break;
-                    case 615:
-                    case 616:
-                        body.style.backgroundImage = 'url("Images/snow_rain.jpg")';
-                        break;
-                    case 620:
-                    case 621:
-                    case 622:
-                        body.style.backgroundImage = 'url("Images/snow_shower.jpg")';
-                        break;
-                }
-            }
-            else if (weatherMain == 'Rain') {
-                let weatherId = response.data.weather[0].id;
-                switch (weatherId) {
-                    case 500:
-                    case 501:
-                        body.style.backgroundImage = 'url("Images/rain.jpg")';
-                        break;
-                    case 503:
-                    case 504:
-                    case 502:
-                    case 520:
-                    case 521:
-                    case 522:
-                    case 531:
-                        body.style.backgroundImage = 'url("Images/heavyrain.jpg")';
-                        break;
-                    case 511:
-                        body.style.backgroundImage = 'url("Images/snow_rain.jpg")';
-                        break;
-                }
-            }
-            else if (weatherMain == 'Thunderstorm') {
-                let weatherId = response.data.weather[0].id;
-                switch (weatherId) {
-                    case 200:
-                    case 201:
-                    case 202:
-                        body.style.backgroundImage = 'url("Images/thunderRain.jpg")';
-                        break;
-                    case 212:
-                    case 221:
-                    case 230:
-                    case 231:
-                    case 232:
-                        body.style.backgroundImage = 'url("Images/heavyThunder.jpg")';
-                        break;
-                    case 210:
-                    case 211:
-                        body.style.backgroundImage = 'url("Images/thunder.jpg")';
-                        break;
-                }
-            }
-            else if (weatherMain == 'Drizzle') {
-                body.style.backgroundImage = 'url("Images/rain.jpg")';
-            }
-            else {
-                body.style.backgroundImage = 'url("Images/base.jpg")';
-            }
+const updateBackground = (weatherMain, weatherId) => {
+    const getImageUrl = (condition, id) => {
+        if (weatherBackgrounds[condition]) {
+            return typeof weatherBackgrounds[condition] === 'string'
+                ? `url("Images/${weatherBackgrounds[condition]}")`
+                : `url("Images/${weatherBackgrounds[condition][id]}")`;
+        }
+        return `url("Images/${weatherBackgrounds.Default}")`;
+    };
 
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-}
+    const imageUrl = getImageUrl(weatherMain, weatherId);
+    document.querySelector("body").style.backgroundImage = imageUrl;
+};
 
-// function getCityCoordinates() {
-//     navigator.geolocation.getCurrentPosition(
-//         position => {
-//             const { latitude, longitude } = position.coords; // Get coordinates of user location
-//             // Get city name from coordinates using reverse geocoding API
-//             const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=6a6cb112b4746fd1d963422db62a0782&q`;
-//             fetch(API_URL).then(response => response.json()).then(data => {
-//                 const { name } = data[0];
-//                 getWeatherDetails(name, latitude, longitude);
-//             }).catch(() => {
-//                 alert("An error occurred while fetching the city name!");
-//             });
-//         },
-//         error => { // Show alert if user denied the location permission
-//             if (error.code === error.PERMISSION_DENIED) {
-//                 alert("Geolocation request denied. Please reset location permission to grant access again.");
-//             } else {
-//                 alert("Geolocation request error. Please reset location permission.");
-//             }
-//         });
-// }
+getWeather();
